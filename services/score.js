472d8280ec26c1d6c836cb473393467664f6376d
rsharoni <rsharoni@gmail.com>
2021-08-18 09:47:03 +0300

    remove logs

--- services/score.js
+++ services/score.js
@@ -3,7 +3,6 @@ require('dotenv/config');
 const SCORE_ID = process.env.SCORE_ID;
 
 exports.getScore = async () => {
-    console.log('get score', SCORE_ID);
     try {
         const scores = await Score.findById(SCORE_ID);
         console.log('scores', scores);
@@ -14,7 +13,6 @@ exports.getScore = async () => {
 }
 
 exports.setScore = async (newScore) => {
-    console.log('set score', SCORE_ID);
     try {
         await Score.updateOne({ _id: SCORE_ID }, { $set: { score: newScore } });
     } catch (err) {
